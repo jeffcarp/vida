62048ed4886e27663300b1b4368a7ff3c86752e9
Jeff Carpenter <gcarpenterv@gmail.com>
2014-07-04 04:16:36 +0000

    Improve testing a bit

--- test/runner.js
+++ test/runner.js
@@ -4,14 +4,14 @@ describe('runner', function(){
 
   var runner = require("../js/runner");
 
-  describe('#createCell()', function(){
+  describe('createCell', function(){
     it('should handle the base case', function() {
 
       var cell = runner.createCell({
         x: 0,
         y: 0,
         ai: "food"
-      }); 
+      });
 
       assert.equal(cell.x, 0);
       assert.equal(cell.y, 0);
